facd5e7bfc6718496a2727dbab0ccf5d11c632bd
Ratul Biswas <istiaqratul@gmail.com>
2019-12-15 19:55:48 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -22,11 +22,11 @@ def won? (board_array)
     third_index_3 = combo_array [2]
 
 
-    symbol_first_array = board_array [first_index_1]
-    symbol_second_array = board_array [second_index_2]
-    symbol_third_array = board_array [third_index_3]
+    symbol_first_index = board_array [first_index_1]
+    symbol_second_index = board_array [second_index_2]
+    symbol_third_index = board_array [third_index_3]
 
-    if symbol_first_array = symbol_second_array && symbol_first_array = symbol_third_array != ""
+    if symbol_first_index = symbol_second_index && symbol_first_index = symbol_third_index != ""
       return combo_array
     end
   end
@@ -43,8 +43,25 @@ def full?(board_array)
     end
   end
 
+  return true
+  
 end
 
 def draw?(board_array)
   if full?(board_array) && !won?(board_array)
     return true
+  else
+    return false
+  end
+end
+
+def over?(board_array)
+  if won?(board_array) || draw?(board_array)
+    return
+  
+end
+
+def winner?(board)
+  if won?(board)
+    
+  
