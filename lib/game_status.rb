856e30d052219d4b0bc12cbd83a80ead44ab0bb1
Ratul Biswas <istiaqratul@gmail.com>
2019-12-15 19:06:55 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,23 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+top_row_win = [0,1,2]
+middle_row_win = [3,4,5]
+bottom_row_win = [6,7,8]
+first_column_win = [0,3,6]
+second_column_win = [1,4,7]
+third_column_win = [2,5,8]
+left_diagnol_win = [0,4,8]
+right_diagnol_win = [2,4,6]
+
+def WIN_COMBINATIONS
+  puts top_row_win
+  puts middle_row_win
+  puts bottom_row_win
+  puts first_column_win
+  puts second_column_win
+  puts third_column_win
+  puts left_diagnol_win
+  puts right_diagnol_win
+end
